192648492c25b3e829901ddfb805402fdf15d254
nathansungu <nathanamudavi@gmail.com>
2025-06-12 19:49:04 +0300

    add dev script and append validation file

--- middleweare/validateTaskDetails.js
+++ middleweare/validateTaskDetails.js
@@ -1,6 +1,6 @@
 const validateTaskDetails =(req, res, next)=>{
     const {title, description, isCompleted} =req.body;
-    if(!title ||  !isCompleted || !description ){
+    if(!title ||  isCompleted == undefined || !description ){
         return res.status(400).json({message: "Provide all the details"})
     }
     next()
