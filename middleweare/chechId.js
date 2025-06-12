cfd445dbe23bc8f355705681a64959d4965f25f1
nathansungu <nathanamudavi@gmail.com>
2025-06-13 01:09:19 +0300

    prittier write

--- middleweare/chechId.js
+++ middleweare/chechId.js
@@ -1,10 +1,9 @@
- const checkId = (req, res, next)=>{
-    const {Id} =req.params;
-    if(!Id){
-        return res.status(400).json({messege: "Provide Id"})
-    }
-    next()
+const checkId = (req, res, next) => {
+  const { Id } = req.params;
+  if (!Id) {
+    return res.status(400).json({ messege: "Provide Id" });
+  }
+  next();
+};
 
-}
-    
 export default checkId;
